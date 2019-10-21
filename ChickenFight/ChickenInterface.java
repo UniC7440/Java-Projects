e44c8e41eafadaf427c6975016a446f7c8321110
UniC <universecodes@gmail.com>
2019-10-21 22:31:22 +0800

    First Project

---
+++ ChickenFight/ChickenInterface.java
@@ -0,0 +1,8 @@
+package cf;
+
+public interface ChickenInterface {
+	public final double maxBet = 100;
+	public final int requiredNumber = 50;
+	public final int maxNumber = 100;
+	public final int chance = 100;
+}
